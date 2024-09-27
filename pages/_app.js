915f29b55259aaab54def8fa572221d9ac1cdc21
Mandy Michael <mandymichael@MandyLaptop.local>
2024-09-27 15:36:05 +0800

    Change to system fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,25 @@
-import '../styles/global.css';
-import localFont from 'next/font/local'
+import "../styles/global.css";
+import localFont from "next/font/local";
 
-const roboto = localFont({ src: './fonts/RobotoFlex.woff2'});
-const roslindale = localFont({ src: './fonts/RoslindaleVariable.woff2'});
+const roboto = localFont({ src: "./fonts/RobotoFlex.woff2" });
+const roslindale = localFont({ src: "./fonts/RoslindaleVariable.woff2" });
 
 function App({ Component, pageProps }) {
   return (
     <>
       <style jsx global>{`
         html {
-          font-family: ${roboto.style.fontFamily};
+          font-family: BlinkMacSystemFont, —apple-system, "Segoe UI",
+            "Oxygen Sans", Ubuntu, sans-serif;
         }
-
         h1 {
-          font-family: ${roslindale.style.fontFamily};
+          font-family: BlinkMacSystemFont, —apple-system, "Segoe UI",
+            "Oxygen Sans", Ubuntu, sans-serif;
         }
       `}</style>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
 export default App;
