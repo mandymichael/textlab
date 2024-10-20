56bf0a279796fec8818dcc422e83ee9fcaa89177
Mandy Michael <mandymichael@MandyLaptop.local>
2024-10-20 14:11:54 +0800

    test prelod

--- _app.js
+++ _app.js
@@ -1,8 +1,18 @@
 import "../styles/global.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="preload"
+          href="/pages/fonts/RobotoFlex.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
