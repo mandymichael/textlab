44b779b81e4301b4801f389d1113dfd89dbea63f
Mandy Michael <mandymichael@MandyLaptop.local>
2024-10-25 14:57:40 +0800

    reset to original for testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,8 @@
 import "../styles/global.css";
-import localFont from "next/font/local";
-
-const roboto = localFont({ src: "./fonts/RobotoFlexSlice.woff2" });
-const roslindale = localFont({
-  src: "./fonts/RoslindaleDisplay-Bold-subset.woff2",
-});
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${roboto.style.fontFamily};
-        }
-
-        h1 {
-          font-family: ${roslindale.style.fontFamily};
-        }
-      `}</style>
       <Component {...pageProps} />
     </>
   );
