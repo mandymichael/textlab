93ef8c2d0e643c4279e0377f43bc91ccee82a5e8
Mandy Michael <mandymichael@MandyLaptop.local>
2024-10-26 13:10:46 +0800

    fix header logo lazy loading

--- components/header.js
+++ components/header.js
@@ -1,20 +1,43 @@
-import HeaderStyles from '../styles/Header.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
+import HeaderStyles from "../styles/Header.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
-    return (
-        <header className={HeaderStyles.headerContainer}>
-            <Link href="/"><Image src="/images/Logo.svg" alt="Text Lab" className={HeaderStyles.logo} width="80" height="80" /></Link>
+  return (
+    <header className={HeaderStyles.headerContainer}>
+      <Link href="/" class={HeaderStyles.logoLink}>
+        <Image
+          src="/images/Logo.svg"
+          alt="Text Lab"
+          className={HeaderStyles.logo}
+          width="80"
+          height="80"
+          loading="eager"
+        />
+      </Link>
 
-            <nav className={HeaderStyles.nav}>
-                <ul className={HeaderStyles.navList}>
-                    <li className={HeaderStyles.navItem}><Link href="/articles" className={HeaderStyles.navLink}>Articles</Link></li>
-                    <li className={HeaderStyles.navItem}><Link href="/about" className={HeaderStyles.navLink}>About</Link></li>
-                </ul>
-            </nav>
+      <nav className={HeaderStyles.nav}>
+        <ul className={HeaderStyles.navList}>
+          <li className={HeaderStyles.navItem}>
+            <Link href="/articles" className={HeaderStyles.navLink}>
+              Articles
+            </Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link href="/about" className={HeaderStyles.navLink}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-            <a href="https://www.linkedin.com/in/mandykerr" target="_blank" className={HeaderStyles.contactLink}>Contact</a>
-        </header>   
-    )
+      <a
+        href="https://www.linkedin.com/in/mandykerr"
+        target="_blank"
+        className={HeaderStyles.contactLink}
+      >
+        Contact
+      </a>
+    </header>
+  );
 }
