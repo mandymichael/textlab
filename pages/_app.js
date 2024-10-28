0bb551763baa9cbce7a3fe0d59ee9235374c6962
Mandy Michael <mandymichael@MandyLaptop.local>
2024-10-28 14:33:38 +0800

    remove scrollRestoration issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import "../styles/global.css";
 import { useEffect } from "react";
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    console.log("scroll");
-    window.history.scrollRestoration = "manual";
-  }, []);
   return (
     <>
       <Component {...pageProps} />
