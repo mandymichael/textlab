ca806cccef4e035e380ef3ac253d4bb40dcf12a1
Mandy Michael <mandymichael@MandyLaptop.local>
2024-11-03 10:21:16 +0800

    preload roslindale font

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "../styles/global.css";
-
+import Head from "next/head";
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="./fonts/RoslindaleDisplay-Bold-subset.woff2"
+          crossorigin
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
